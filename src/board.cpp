e34414081b0b1e7a84f0101b4300838dbbdb0349
ForestFe <60042426+ForestFe@users.noreply.github.com>
2021-07-14 20:18:38 +0900

    Update board.cpp

--- src/board.cpp
+++ src/board.cpp
@@ -1,68 +1,140 @@
-#include "board.h"
+#pragma once
+#include <iostream>
+#include<algorithm>
+#include <vector>
 
-bool Board::find(const Point& start, const Point& goal)
-{
-	mass_[start.y()][start.x()].setStatus(Mass::START);
-	mass_[goal.y()][goal.x()].setStatus(Mass::GOAL);
 
-	Point p = start;
-	while (p != goal) {
-		if (p != start) mass_[p.x()][p.y()].setStatus(Mass::WAYPOINT);
+class Point {
+	int x_ = -1,
+		y_ = -1;
+public:
+	Point(){}
+	Point(int x, int y) :x_(x), y_(y) {}
+	int x() const { return x_; }
+	int y() const { return y_; }
+	void setX(int x) { x_ = x; }
+	void setY(int y) { y_ = y; }
+	void set(int x, int y) { x_ = x; y_ = y; }
 
-		if (p.x() < goal.x()) { p.setX(p.x() + 1); continue; }
-		if (goal.x() < p.x()) { p.setX(p.x() - 1); continue; }
-		if (p.y() < goal.y()) { p.setY(p.y() + 1); continue; }
-		if (goal.y() < p.y()) { p.setY(p.y() - 1); continue; }
+	bool operator == (const Point& p) const {
+		return p.x() == x_ && p.y() == y_;
+	}
+	bool operator != (const Point& p) const {
+		return !(p == *this);
 	}
 
-	return false;
+	Point getRight() const { return Point(x_ + 1, y_); }
+	Point getLeft () const { return Point(x_ - 1, y_); }
+	Point getUp   () const { return Point(x_, y_ - 1); }
+	Point getDown () const { return Point(x_, y_ + 1); }
+
+	static double distance(const Point p1, const Point p2) {
+		double dx = (double)p2.x() - (double)p1.x();
+		double dy = (double)p2.y() - (double)p1.y();
+		return sqrt(dx * dx + dy * dy);
+	}
+};
+
+class Mass {
+public:
+	enum status {
+		BLANK,
+		GOAL,
+		START,
+		WAYPOINT,
+		WALL, // 通れない
+		WATER,// 進むのが1/3に遅くなる
+		ROAD,//進むのが3倍速い
+	};
+	enum listed {
+		NONE,
+		OPNE,
+		CLOSE,
+	};
+private:
+	status s_ = BLANK;
+	listed listed_ = NONE;
+	Point pos_;
+	Mass* pParent_ = nullptr;
+	int steps_ = 0;
+	double estimate_ = 0.0;
+
+	static int getWalkCost(Mass& m) { status s = m.getStatus(); return(s == WATER) ? 3 : (s == ROAD) ? -3 : 1; }
+
+	void calcCost(const Point target) {
+		steps_ = (pParent_ ? pParent_->steps_ : 0) + getWalkCost(*this);
+		estimate_ = 3.0 * Point::distance(pos_, target);
+	}
+public:
+	void setStatus(status s) { s_ = s; }
+	status getStatus() const { return s_; }
+
+	void setPos(int x, int y) { pos_.set(x, y); }
+	const Point& getPos() const { return pos_; }
+	int x() { return pos_.x(); }
+	int y() { return pos_.y(); }
+
+	void setParent(Mass* pParent, const Point& goal) { pParent_ = pParent; calcCost(goal); }
+	Mass* getParent() { return pParent_; }
+
+	void setListed(listed t) { listed_ = t; }
+	bool isListed(listed t) const { return listed_ == t; }
+
+	double getCost()const { return (double)steps_ + estimate_; }
+};
+
+
+bool static asc(const Mass* o1, const Mass* o2) {
+	return o1->getCost() < o2->getCost();
 }
 
-void Board::show() const 
-{
-	std::cout << std::endl;
+class Board {
+private:
+	enum {
+		BOARD_SIZE = 10,
+	};
+	Mass mass_[BOARD_SIZE][BOARD_SIZE];
+	Mass &getMass(const Point p) { return mass_[p.y()][p.x()]; };
 
-	for (int y = 0; y < BOARD_SIZE; y++) {
-		std::cout << " ";
-		for (int x = 0; x < BOARD_SIZE; x++) {
-			std::cout << "+-";
-		}
-		std::cout << "+" << std::endl;
-
-		std::cout << " ";
-		for (int x = 0; x < BOARD_SIZE; x++) {
-			std::cout << "|";
-			switch (mass_[y][x].getStatus()) {
-			case Mass::BLANK:
-				std::cout << " ";
-				break;
-			case Mass::START:
-				std::cout << "S";
-				break;
-			case Mass::GOAL:
-				std::cout << "G";
-				break;
-			case Mass::WAYPOINT:
-				std::cout << "o";
-				break;
-			case Mass::WALL:
-				std::cout << "#";
-				break;
-			case Mass::WATER:
-				std::cout << "~";
-				break;
-			case Mass::ROAD:
-				std::cout << "$";
-				break;
+	std::vector<Mass*> open_list_;
+public:
+	Board() {
+		for (int y = 0; y < BOARD_SIZE; y++) {
+			for (int x = 0; x < BOARD_SIZE; x++) {
+				//mass_[y][x].setStatus(Mass::BLANK);
+				mass_[y][x].setPos(x, y);
 			}
 		}
-		std::cout << "|" << std::endl;
+		// 壁
+		mass_[4][6].setStatus(Mass::WALL);
+		mass_[4][5].setStatus(Mass::WALL);
+		mass_[5][5].setStatus(Mass::WALL);
+		mass_[6][5].setStatus(Mass::WALL);
+		// 水
+		for (int y = 4; y <= 7; y++) {
+			for (int x = 1; x <= 4; x++) {
+				mass_[y][x].setStatus(Mass::WATER);
+			}
+		}
+		// 道
+		mass_[4][1].setStatus(Mass::ROAD);
+		mass_[5][1].setStatus(Mass::ROAD);
+		mass_[5][2].setStatus(Mass::ROAD);
+		mass_[5][3].setStatus(Mass::ROAD);
+		mass_[6][1].setStatus(Mass::ROAD);
+		mass_[6][2].setStatus(Mass::ROAD);
+		mass_[6][3].setStatus(Mass::ROAD);
+	}
+	~Board() {}
+	void addWall(const Point& p) { getMass(p).setStatus(Mass::WALL); }
+
+	bool isValidated(const Point& p) {
+		if (getMass(p).getStatus() == Mass::WALL) {
+			return false;
+		}
 	}
 
-	std::cout << " ";
-	for (int x = 0; x < BOARD_SIZE; x++) {
-		std::cout << "+-";
-	}
-	std::cout << "+" << std::endl;
+	bool find(const Point& start, const Point& goal);
 
-}
+	void show() const;
+};
